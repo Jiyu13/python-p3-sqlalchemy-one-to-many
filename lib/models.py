245f8d7d673c92137c2d177343cdd7eb3b6ee4d8
Ziru Zou <jiyucoding@yahoo.com>
2023-03-06 10:56:14 -0500

    update

--- lib/models.py
+++ lib/models.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 # 1. Create Game Model
 class Game(Base):
 
-    __tablename__ = "games"
+    __tablename__ = 'games'
 
     id = Column(Integer(), primary_key=True)
     title = Column(String())
@@ -21,14 +21,14 @@ class Game(Base):
     price = Column(Integer())
 
     # 4. add the relationship to Game
-    reviews = relationship("Review", backref=backref("game"))
+    reviews = relationship('Review', backref=backref('game'))
 
 
     # 5. set __repr__()
     def __repr__(self):
-        return f"Game(id={self.id}), " + \
-            f"title={self.title}, " + \
-            f"platform={self.platform}, "
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
 
 
 
@@ -39,12 +39,12 @@ class Game(Base):
 # 3. Create Review Model and associate it with Game using foreign key
 class Review(Base):
 
-    __tablename__ = "reviews"
+    __tablename__ = 'reviews'
 
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
-    game_id = Column(Integer(), ForeignKey("games.id"))
+    game_id = Column(Integer(), ForeignKey('games.id'))
 
     # set __repr__()
     def __repr__(self):
